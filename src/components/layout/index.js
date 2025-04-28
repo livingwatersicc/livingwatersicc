09ab30656e71976fac28ce83aea88c159b83cf6d
Robin Arulanantham <robin.arulanantham@infometrics.co.nz>
2025-04-29 09:11:52 +1200

    Fix scroll issue

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,15 +1,25 @@
 import { Footer } from "components/footer";
 import { NavMenu } from "components/nav";
-import styles from "./layout.module.scss";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 export const Layout = ({ children }) => {
+  const ref = useRef();
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ block: "start" });
+    }
+  }, [pathname]);
+
   return (
-    <>
+    <div ref={ref}>
       <NavMenu />
       <main>{children}</main>
       <footer className="bg-primary">
         <Footer />
       </footer>
-    </>
+    </div>
   );
 };
