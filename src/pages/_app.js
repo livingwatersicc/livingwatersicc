1544747b29f504edd0101543920ad8741ad0b641
Robin Arulanantham <robin.arulanantham@infometrics.co.nz>
2025-05-10 13:11:55 +1200

    Add Google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import { Layout } from "components/layout";
 
@@ -9,10 +10,15 @@ config.autoAddCss = false;
 import "styles/globals.scss";
 import "styles/fonts.scss";
 
+export const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <GoogleAnalytics gaId={gaId} />
+    </>
   );
 }
